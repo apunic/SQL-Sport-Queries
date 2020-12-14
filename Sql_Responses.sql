a11c294a120287b319c3526639873d3ab9274480
Alina Popescu <alina.business.analyst@gmail.com>
2020-12-14 12:37:16 +0000

    Add questions 24-30

--- Sql_Responses.sql
+++ Sql_Responses.sql
@@ -21,3 +21,15 @@ Select ciclista.dorsal, ciclista.nombre from ciclista, puerto where ciclista.dor
 Select ciclista.nombre from ciclista where ciclista.edad= (select min(edad)from ciclista )
 SELECT C1.nombre FROM ciclista C1 WHERE C1.edad = (SELECT MIN(C2.edad)FROM ciclista C2 WHERE C2.dorsal IN (SELECT etapa.dorsal FROM etapa) AND C1.dorsal IN (SELECT etapa.dorsal from etapa ))
 Select Distinct C.nombre from ciclista C,puerto P where 1 < (select count (*) from puerto P2 where C.dorsal= P2.dorsal)
+Select  E.netapa from etapa E where not exists (select * from puerto P where E.netapa=P.netapa and P.altura<=700 ) AND ExISTS (SELECT * FROM PUERTO P WHERE E.NETAPA=P.NETAPA AND P.ALTURA>700)
+SELECT NOMEQ,DIRECTOR FROM EQUIPO E WHERE not exists(SELECT * FROM CICLISTA  C WHERE E.nomeq=C.nomeq and C.edad <=25) and exists(select * from ciclista C2 where E.nomeq=C2.nomeq and C2.edad>25)
+Select C.dorsal,C.nombre from ciclista C where not exists (select * from etapa E1 where C.dorsal= E1.dorsal and E1.Km <=170) and exists(select* from Etapa E2 where E2.dorsal= C.dorsal and E2.km >170)
+Select C.nombre from ciclista C where exists(select*from etapa E where (E.dorsal= C.dorsal  and not exists( select* from Puerto P where P.netapa=E.netapa and P.dorsal<>C.dorsal) and exists(select* from Puerto P where P.netapa=E.netapa)))
+
+SELECT E.nomeq FROM EQUIPO E WHERE NOT EXISTS (SELECT *FROM CICLISTA C WHERE NOT EXISTS (  SELECT * FROM LLEVAR L WHERE C.dorsal=L.dorsal )
+AND NOT EXISTS ( SELECT *FROM PUERTO P WHERE C.dorsal=P.dorsal ) AND C.nomeq=E.nomeq )
+AND EXISTS (SELECT * FROM CICLISTA C WHERE C.nomeq=E.nomeq)
+
+Select Distinct M.color, M.codigo from Maillot M ,Ciclista C,llevar L where L.codigo= M.codigo and C.dorsal=L.dorsal and not exists(Select* from Ciclista C1,LLEVAR L1 where C.nomeq<>C1.nomeq and L1.dorsal=C1.dorsal and L1.codigo=M.codigo)
+Select distinct C.nomeq from ciclista C , puerto P where C.dorsal=P.dorsal and P.categoria='1'  and  not exists (Select * from Ciclista C1,Puerto P1 where C.nomeq= C1.nomeq and C1.dorsal=P1.dorsal and P1.categoria <>'1')
+
